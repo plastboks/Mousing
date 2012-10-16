718ea0d13039e1cdebe77c06cb6f694d2353ccfb
Alexander Skjolden <alex@plastboks.net>
2012-10-16 09:50:25 +0200

    now with ncurses

--- mousing.c
+++ mousing.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
-#include <stdio.h>   
+#include <stdio.h>
+#include <ncurses.h>
 #include <linux/input.h>
 #include <fcntl.h>    
+
+#define VERSION 0.01
 
 
 int main(int argc, char **argv) {
   unsigned long mouseMovement = 0;
-
   int fd;
+
+  initscr();
+
   if ((fd = open("/dev/input/mice", O_RDONLY)) < 0) {
     perror("evdev open");
     exit(1);
@@ -18,9 +23,10 @@ int main(int argc, char **argv) {
   while(1) {
     if (read(fd, &ev, sizeof(struct input_event))) {
       mouseMovement++;
+      printw("Length: %lu \r", mouseMovement);
+      refresh();
     }
-    printf("Length: %lu \r", mouseMovement);
-    fflush(stdout);
+  
   }
 
   return 0;
