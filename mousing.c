854bf37ce01d97faac321ec5a9cfda96896432ff
Alexander Skjolden <alex@plastboks.net>
2013-02-23 12:36:51 +0100

    version 0.0.2

--- mousing.c
+++ mousing.c
@@ -7,32 +7,22 @@
 #include <linux/input.h>
 #include <fcntl.h>
 
-
-#define VERSION 0.01
+#define VERSION 0.02
 #define MOUSEFILE "/dev/input/mice"
 
 #define LEFTCLICK 9
 #define RIGHTCLICK 10
 
+struct input_event ev;
 
-int main() {
+unsigned long int mouseMovement = 0;
+unsigned int leftMouseClick = 0;
+unsigned int rightMouseClick = 0;
 
-  unsigned long int mouseMovement = 0;
-  unsigned int leftMouseClick = 0;
-  unsigned int rightMouseClick = 0;
-  int fd;
 
-  initscr();
-  curs_set(0);
-
-  struct input_event ev;
-
-  if((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
-    perror("evdev open");
-    exit(1);
-  }
-
-  while(read(fd, &ev, sizeof(struct input_event))) {
+void readMouse(int fd) {
+  
+  if(read(fd, &ev, sizeof(struct input_event))) {
 
     switch( ev.time.tv_sec ) {
       case LEFTCLICK:
@@ -44,13 +34,32 @@ int main() {
       default :
         mouseMovement++;
     }
+  }
 
-    printw("Left click: %d, ", leftMouseClick);
-    printw("Right click: %d, ", rightMouseClick);
-    printw("Movement:  %d \r", mouseMovement);
+}
+
+
+int main() {
+
+  int fd;
+
+  initscr();
+  curs_set(0);
+
+
+  if ((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
+    perror("evdev open");
+    exit(1);
+  }
+
+  while (true) { 
+    readMouse(fd);
+
+    mvprintw(0,0, "Left click: %d, ", leftMouseClick);
+    mvprintw(1,0, "Right click: %d, ", rightMouseClick);
+    mvprintw(2,0, "Mousemovement: %d ", mouseMovement);
     refresh();
   }
 
   return 0;
 }
-
